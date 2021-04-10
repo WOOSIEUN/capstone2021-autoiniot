c1c25466ed7700629245edd492caf5b9a9c4089a
SIEUN WOO <ramtk6726@naver.com>
2021-04-10 15:42:07 +0900

    feat: Init web server for Autoinven.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express();
+
+const server = app.listen(4000, () => {
+    console.log('Start Server : localhost:4000');
+});
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function (req, res){
+    res.render('index.html')
+})
+
+app.get('/info', function (req, res){
+    res.render('info page')
+})
