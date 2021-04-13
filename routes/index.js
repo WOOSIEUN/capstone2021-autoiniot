0e2eb643bbece03ce7868f030774c38f4dcd88a1
WOOSIEUN <ramtk6726@naver.com>
2021-04-13 19:38:05 +0900

    feat: Create html file for project

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,12 @@ router.get('/', function (req, res, next){
   res.render('index.html')
 })
 
-router.get('/main', function (req, res, next){
-  res.render('main.html')
+router.get('/main_realtime_warehouse', function (req, res, next){
+  res.render('main_realtime_warehouse.html')
+})
+
+router.get('/main_warehousing', function (req, res, next){
+  res.render('main_warehousing.html')
 })
 
 module.exports = router;
