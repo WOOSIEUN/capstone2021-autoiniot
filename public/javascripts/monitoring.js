1a6c733a6f50a1677b65a3d85d0cbef760340d64
abcworld123 <48833198+abcworld123@users.noreply.github.com>
2021-06-04 21:15:06 +0900

    style: Change some charts

--- public/javascripts/monitoring.js
+++ public/javascripts/monitoring.js
@@ -11,10 +11,10 @@ $(function() {
 	var config1 = {
 		type: 'line',
 		data: {
-			labels: [0, 4, 8, 12, 16, 20, 24],
+			labels: ['', '', '', '', '', '', ''],
 			datasets: [{
 				label: '온도',
-				data: [13, 10, 8, 15, 19, 18, 15],
+				data: [0, 0, 0, 0, 0, 0, 0],
 				fill: false,
 				borderColor: 'orange',
 				tension: 0.1
@@ -24,7 +24,7 @@ $(function() {
 			scales: {
 				y: {
 					min: 0,
-					max: 30
+					max: 40
 				}
 			},
 			plugins: {
@@ -45,10 +45,10 @@ $(function() {
 	var config2 = {
 		type: 'line',
 		data: {
-			labels: [0, 4, 8, 12, 16, 20, 24],
+			labels: ['', '', '', '', '', '', ''],
 			datasets: [{
 				label: '습도',
-				data: [13, 10, 8, 15, 19, 18, 15],
+				data: [0, 0, 0, 0, 0, 0, 0],
 				fill: false,
 				borderColor: 'blue',
 				tension: 0.1
@@ -133,128 +133,6 @@ $(function() {
 			},
 		}
 	};
-
-
-
-	// var config1 = {
-	// 	type: 'bar',
-	// 	data: {
-	// 		labels: ['11월', '12월', '1월', '2월', '3월', '4월'],
-	// 		datasets: [{
-	// 			data: [8, 4, 13, 5, 19, 14],
-	// 			label: '입고',
-	// 			backgroundColor: [
-	// 				'rgba(255, 99, 132, 0.2)',
-	// 				'rgba(54, 162, 235, 0.2)',
-	// 				'rgba(255, 206, 86, 0.2)',
-	// 				'rgba(75, 192, 192, 0.2)',
-	// 				'rgba(153, 102, 255, 0.2)',
-	// 				'rgba(255, 159, 64, 0.2)'
-	// 			],
-	// 			borderColor: [
-	// 				'rgba(255, 99, 132, 1)',
-	// 				'rgba(54, 162, 235, 1)',
-	// 				'rgba(255, 206, 86, 1)',
-	// 				'rgba(75, 192, 192, 1)',
-	// 				'rgba(153, 102, 255, 1)',
-	// 				'rgba(255, 159, 64, 1)'
-	// 			],
-	// 			borderWidth: 1
-	// 		}]
-	// 	},
-	// 	options: {
-	// 		scales: {
-	// 			y: {
-	// 				beginAtZero: true
-	// 			}
-	// 		},
-	// 		plugins: {
-	// 			legend: {
-	// 				display: false
-	// 			},
-	// 			title: {
-	// 				display: true,
-	// 				text: '월별 입고 현황',
-	// 				font: {
-	// 					size: 25
-	// 				}
-
-	// 			},
-	// 		},
-	// 	}
-	// };
-
-	// var config3 = {
-	// 	type: 'doughnut',
-	// 	data: {
-	// 		labels: [ 'Red', 'Blue', 'Yellow'],
-	// 		datasets: [{
-	// 			label: '1234',
-	// 			data: [300, 50, 100],
-	// 			backgroundColor: [
-	// 				'rgb(255, 99, 132)',
-	// 				'rgb(54, 162, 235)',
-	// 				'rgb(255, 205, 86)'
-	// 			],
-	// 			hoverOffset: 4
-	// 		}]
-	// 	},
-	// 	options: {
-	// 		plugins: {
-	// 			legend: {
-	// 				display: false
-	// 			},
-	// 			title: {
-	// 				display: true,
-	// 				text: ' 물품수량 통계',
-	// 				font: {
-	// 					size: 25
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// };
-
-	// var config4 = {
-	// 	type: 'bar',
-	// 	data: {
-	// 		labels: ['11월', '12월', '1월', '2월', '3월', '4월'],
-	// 		datasets: [
-	// 			{
-	// 				data: [10, 0, 7, 0, 23, 18],
-	// 				backgroundColor: '#ff6484',
-	// 			},
-	// 			{
-	// 				data: [0, -16, 0, -6, 0, 0],
-	// 				backgroundColor: '#38a2ea',
-	// 			}
-	// 		]
-	// 	},
-	// 	options: {
-	// 		plugins: {
-	// 			legend: {
-	// 				display: false
-	// 			},
-	// 			title: {
-	// 				display: true,
-	// 				text: '월별 영업이익',
-	// 				font: {
-	// 					size: 25
-	// 				}
-
-	// 			},
-	// 		},
-	// 		responsive: true,
-	// 		scales: {
-	// 			x: {
-	// 				stacked: true,
-	// 			},
-	// 			y: {
-	// 				stacked: true
-	// 			}
-	// 		}
-	// 	}
-	// };
 
 	// 차트 생성
 	var chart1 = new Chart(ctx1, config1);
